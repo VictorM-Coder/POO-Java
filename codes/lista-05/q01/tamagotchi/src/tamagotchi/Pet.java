1eb0a878bba06d7d5755816a52df5e52ae45bbc6
Victor Martins <martinsvictor6781@gmail.com>
2021-11-08 14:53:42 -0300

    inicialização do código do tamagotchi - lista 5

--- codes/lista-05/q01/tamagotchi/src/tamagotchi/Pet.java
+++ codes/lista-05/q01/tamagotchi/src/tamagotchi/Pet.java
@@ -1,11 +1,57 @@
 package tamagotchi;
 
-class Pet {
+public class Pet {
     private int energyMax, hungryMax, cleanMax;
-    private int energy, hungry, shower;
+    private int energy, hungry, clean;//troquei o nome da variável shower pr clean para manter o padrão
     private int diamonds;
     private int age;
     private boolean alive;
+
+    public Pet(int energy, int hungry, int clean){
+        this.energyMax = energy;
+        this.hungryMax = hungry;
+        this.cleanMax = clean;
+        
+        this.setEnergy(energy);
+        this.setHungry(hungry);
+        this.setClean(clean);
+        
+        this.age = 0;
+        this.diamonds = 0;
+    }
+
+    public String toString(){
+        return "E:" + this.energy + "/" + this.energyMax + ", S:" + this.hungry + "/" + this.hungryMax + ", L:"
+                + this.clean + "/" + this.cleanMax + ", D:" + this.diamonds + ", I:" + this.age;
+    }
+    
+    // Invoca o método testAlive para verificar se o pet esta vivo
+    // Se estiver vivo, altere os atributos utilizando os métodos set e get
+    public void play(){
+
+    }
+
+    public void shower(){
+
+    }
+
+    public void eat(){
+
+    }
+
+    public void sleep(){
+
+    }
+
+    private int returnAtributeBasedOnMax(int value, int maxValue){
+        if (value <= 0){
+            return 0;
+        }else if (value > maxValue){
+            return maxValue;
+        }else{
+            return value;
+        }
+    }
 
     // Atribui o valor de energia
     // Se o valor ficar abaixo de 0, o pet morre de fraqueza
@@ -23,38 +69,54 @@ class Pet {
     }
 
     void setHungry(int value){
+       this.hungry = this.returnAtributeBasedOnMax(value, this.hungryMax);
 
+       if (this.hungry == 0){
+           this.alive = false;
+           System.out.println("fail: pet morreu de fome");
+       }
     }
 
     void setClean(int value){
+        this.clean = this.returnAtributeBasedOnMax(value, this.hungryMax);
 
+        if (this.clean == 0){
+            this.alive = false;
+            System.out.println("fail: pet morreu de sujeira");
+        }
     }
 
-    public Pet(int energy, int hungry, int shower);
+    int getClean(){
+        return this.clean;
+    }
 
-    int getClean();
+    int getHungry(){
+        return this.hungry;
+    }
 
-    int getHungry();
+    int getEnergy(){
+        return this.energy;
+    }
 
-    int getEnergy();
+    int getEnergyMax(){
+        return this.energyMax;
+    }
 
-    int getEnergyMax();
+    int getCleanMax(){
+        return this.cleanMax;
+    }
 
-    int getCleanMax();
+    int getHungryMax(){
+        return this.hungryMax;
+    }
 
-    int getHungryMax();
+    public boolean testAlive(){
+        if (!this.alive){
+            System.out.println("fail: pet esta morto");
+        }
+        
+        return this.alive;
+    }
 
-    public String toString();
 
-    public boolean testAlive();
-
-    // Invoca o método testAlive para verificar se o pet esta vivo
-    // Se estiver vivo, altere os atributos utilizando os métodos set e get
-    public void play();
-
-    public void shower();
-
-    public void eat();
-
-    public void sleep();
 }
